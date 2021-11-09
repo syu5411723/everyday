f88d2191f3105567e000299734e94985f64eb8fa
maekawa-syuki <syu54117123>
2021-11-09 12:10:36 +0900

    add com06

--- components/pages/home.tsx
+++ components/pages/home.tsx
@@ -1,10 +1,12 @@
 ﻿import React from 'react'
 import { Sec01 } from '../organisms/main/Sec01'
+import { Sec02 } from '../organisms/main/Sec02'
 
 export const Home = () => {
     return (
         <>
-            <Sec01 />1
+            <Sec01 />
+            <Sec02 />
         </>
     )
 }
