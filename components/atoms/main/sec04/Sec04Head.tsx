6abeb4ce103735d350d6e58b48a3c15754934fa6
maekawa-syuki <syu54117123>
2021-11-13 10:48:51 +0900

    add com08

---
+++ components/atoms/main/sec04/Sec04Head.tsx
@@ -0,0 +1,19 @@
+﻿import styled from "styled-components"
+
+const Wrapper = styled.div`
+    
+`
+const Head = styled.h2`
+    margin-bottom: 30px;
+`
+const Content = styled.p``
+
+export const Sec04Head = () => {
+    return (
+        <Wrapper>
+            <Head>2022 Calendars</Head>
+            <Content>ゴッホのポケットカレンダーやアートのインポート</Content>
+            <Content>カレンダー、卓上カレンダーなど幅広くラインナップ</Content>
+        </Wrapper>
+    )
+}
---
+++ components/molcules/main/sec04/Sec04Left.tsx
@@ -0,0 +1,16 @@
+﻿import styled from "styled-components"
+import { Sec04Head } from "../../../atoms/main/sec04/Sec04Head"
+import { Button } from "../../../design/Button"
+
+const Wrapper = styled.div`
+    
+`
+
+export const Sec04Left = () => {
+    return (
+        <Wrapper>
+            <Sec04Head />
+            <Button text="Shop 2022 Calendars" />
+        </Wrapper>
+    )
+}
