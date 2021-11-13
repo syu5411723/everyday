47946750dbe9344fd50d2b59f2c734809bb6ecb1
maekawa-syuki <syu54117123>
2021-11-14 08:57:27 +0900

    add com09

---
+++ components/atoms/main/sec04/Sec04Img.tsx
@@ -0,0 +1,14 @@
+﻿import Image from "next/image"
+import styled from "styled-components"
+
+const Wrapper = styled.div`
+    position:relative;
+`
+
+export const Sec04Img = () => {
+    return (
+        <Wrapper>
+            <Image src="/"  layout="fill" objectFit="cover"/>
+        </Wrapper>
+    )
+}
