d1149a1bbfaf1ea157cb1a0dd2966660b89799ca
maekawa-syuki <syu54117123>
2021-11-15 09:23:53 +0900

    add com10

---
+++ components/atoms/main/sec05/Sec05Head.tsx
@@ -0,0 +1,22 @@
+﻿import styled from "styled-components"
+
+const Wrapper = styled.div`
+    
+`
+const Head = styled.h2`
+    margin-bottom: 30px;
+`
+const SubHead = styled.h3``
+const Content = styled.p``
+
+export const Sec05Head = () => {
+    return (
+        <Wrapper>
+            <Head>Discover what sets</Head>
+            <Head>MoMA Design</Head>
+            <Head>Store apart</Head>
+            <SubHead>MoMAデザインストアの独自性</SubHead>
+            <Content>MoMaデザインがどのように承認を選定しているのか。その独自の視点や、何が特別かをご紹介します。</Content>
+        </Wrapper>
+    )
+}
