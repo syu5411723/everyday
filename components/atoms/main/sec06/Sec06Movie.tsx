ff55027c7aa6de07a367c5f89836b411aadada05
maekawa-syuki <syu54117123>
2021-11-17 07:20:53 +0900

    commit 12

---
+++ components/atoms/main/sec06/Sec06Movie.tsx
@@ -0,0 +1,15 @@
+﻿import Image from "next/image"
+import styled from "styled-components"
+
+const Wrapper = styled.div`
+    position:relative;
+`
+
+
+export const Sec06Movie = () => {
+    return (
+        <Wrapper>
+            <Image src="/"  layout="fill" objectFit="cover"/>
+        </Wrapper>
+    )
+}
