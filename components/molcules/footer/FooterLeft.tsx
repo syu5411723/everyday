1e4e2c40586a167c529e6b14b6ee0b72d5d26cca
maekawa-syuki <syu54117123>
2021-11-21 06:40:43 +0900

    add com

---
+++ components/molcules/footer/FooterLeft.tsx
@@ -0,0 +1,17 @@
+﻿import styled from "styled-components"
+import { FooterAnnounce } from "../../atoms/footer/FooterAnnounce"
+
+const Wrapper = styled.div`
+
+`
+const Inner = styled.div``
+
+export const FooterLeft = () => {
+    return (
+        <Wrapper>
+            <Inner>
+                <FooterAnnounce />
+            </Inner>
+        </Wrapper>
+    )
+}
