6556ced1b17c89c52c5db3e571c247b1329279be
maekawa-syuki <syu54117123>
2021-11-23 08:48:04 +0900

    add com

--- components/pages/home.tsx
+++ components/pages/home.tsx
@@ -5,6 +5,7 @@ import { Sec02 } from '../organisms/main/Sec02'
 import { Sec03 } from '../organisms/main/Sec03'
 import { Sec04 } from '../organisms/main/Sec04'
 import { Sec05 } from '../organisms/main/Sec05'
+import { Sec06 } from '../organisms/main/Sec06'
 import { Sec07 } from '../organisms/main/Sec07'
 
 export const Home = () => {
@@ -15,6 +16,7 @@ export const Home = () => {
             <Sec03 />
             <Sec04 />
             <Sec05 />
+            <Sec06 />
             <Sec07 />
             <Footer />
         </>
