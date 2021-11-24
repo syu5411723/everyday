6729f2518873997376157770f2adc90a5500f3ac
maekawa-syuki <syu54117123>
2021-11-24 11:42:36 +0900

    add data

---
+++ components/atoms/footer/FooterRightItem.tsx
@@ -0,0 +1,22 @@
+﻿import styled from "styled-components"
+import { VFC } from "react"
+
+
+type Props = {
+    Data: any
+    title: string
+}
+const Wrapper = styled.div`
+
+`
+const Title = styled.p``
+const Text = styled.p``
+
+export const FooterRightItem: VFC<Props> = ({ Data, title }) => {
+    return (
+        <Wrapper>
+            <Title>{title}</Title>
+            {Data.map(data => <Text>{data.text}</Text>)}
+        </Wrapper>
+    )
+}
