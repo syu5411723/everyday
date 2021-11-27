e77dc393d3a0dee746fc0d1f4a756aeb8925869e
maekawa-syuki <syu54117123>
2021-11-28 07:37:00 +0900

    add context

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,31 @@
 import { Layout } from '../components/design/Layout'
 import { Home } from '../components/pages/Home'
+import { createContext } from "react"
+import Head from "next/head"
+
+type ContextProps = {
+  data: any
+}
 
 const Data = [{}]
 
 export const getStaticProps = async () => {
   const data = await Data
-  return { props:data }
+  return { props: data }
 }
 
-const index = ({data}) => {
+export const DataContext = createContext({} as ContextProps)
+
+const index = ({ data }) => {
   return (
     <>
-      <Home />
-      </>
+      <Head>
+        <title></title>
+      </Head>
+      <DataContext.Provider value={{data}} >
+        <Home />
+      </DataContext.Provider>
+    </>
   )
 }
 
