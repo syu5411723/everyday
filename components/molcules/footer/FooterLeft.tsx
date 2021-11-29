7bb9930062cb2a7a4a01119d1a2e3263190081d1
maekawa-syuki <syu54117123>
2021-11-30 08:41:28 +0900

    edit context

--- components/molcules/footer/FooterLeft.tsx
+++ components/molcules/footer/FooterLeft.tsx
@@ -1,6 +1,10 @@
 ﻿import styled from "styled-components"
+import { useContext } from "react"
+
 import { FooterAnnounce } from "../../atoms/footer/FooterAnnounce"
 import { FooterRightItem } from "../../atoms/footer/FooterRightItem"
+import {DataContext} from "../../../pages/index"
+
 
 const data = {
     id: 1,
@@ -12,7 +16,9 @@ const Wrapper = styled.div`
 `
 const Inner = styled.div``
 
+
 export const FooterLeft = () => {
+    const {data} = useContext(DataContext)
     return (
         <Wrapper>
             <Inner>
