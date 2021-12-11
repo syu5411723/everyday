27cb16c2702bb510100635a1809a143f714b939f
maekawa-syuki <syu54117123>
2021-12-12 07:28:08 +0900

    add detail

--- components/organisms/main/detail/DetalLeft.tsx
+++ components/organisms/main/detail/DetalLeft.tsx
@@ -1,11 +1,14 @@
-﻿import {DetailContext} from "../../../../pages/posts/[id]"
-import {useContext} from "react"
+﻿import { DetailContext } from "../../../../pages/posts/[id]"
+import { useContext } from "react"
+import { DetailItem } from "../../../atoms/main/detail/DetailItem"
+import { DetailTitle } from "../../../atoms/main/detail/DetailTitle"
 
 export const DetalLeft = () => {
     const { data } = useContext(DetailContext)
     return (
         <>
-            
+            <DetailTitle />
+            <DetailItem />
         </>
     )
 }
